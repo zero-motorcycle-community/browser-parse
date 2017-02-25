d7f126c21abc137bdb3d633a89dce2ba1d7098e8
keithxemi <keith@ivyriver.net>
2017-02-25 09:13:52 -0500

    use powershell script to merge

---
+++ merge.ps1
@@ -0,0 +1,1 @@
+Get-Content .\components\htm1.txt, .\components\styles.css, .\components\htm2.txt, .\components\send-logs-base64-png.txt, .\components\htm3.txt, .\components\FileSaver.js, .\components\htm4.txt, .\components\zero-parse.js, .\components\htm5.txt | Out-File .\zero-log-parser.html -Encoding UTF8
